6eb213606164ecf2d47e4cda36f8ff61ffaececc
joao pedro mendes silva <joaopedromendessilva@gmail.com>
2022-10-29 16:23:12 -0300

    CRUD COMPLETO

--- src/main/java/com/springboot/controller/PessoaController.java
+++ src/main/java/com/springboot/controller/PessoaController.java
@@ -2,15 +2,14 @@ package com.springboot.controller;
 
 import java.util.Optional;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springboot.model.Pessoa;
@@ -27,19 +26,24 @@ public class PessoaController {
 	 public ModelAndView inicio() {
     	 
     	 ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
-    	 andView.addObject("pessoaObj", new Pessoa());
+    	 Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+    	 andView.addObject("pessoas", pessoasIt);
+    	 andView.addObject("pessoaObj", new Pessoa()); //inicia a tela com obj vazio, pois form espera um objeto
     
     	 return andView; 
 	 }
      
-     @RequestMapping(method=RequestMethod.POST, value="/salvarpessoa") //intercepta os dados pela url e injeta no objeto pessoa
+     @RequestMapping(method = RequestMethod.POST, value="**/salvarpessoa") //intercepta os dados pela url e injeta no objeto pessoa
      public ModelAndView salvar(Pessoa pessoa) {
     	 
     	pessoaRepository.save(pessoa);
     	
-	   	ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
-	   	Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
-		andView.addObject("pessoas", pessoasIt);
+	   	ModelAndView andView = new ModelAndView("cadastro/cadastropessoa.html");
+	   	
+	   	Iterable<Pessoa> pessoasIt = pessoaRepository.findAll(); //retorna a lista após salvar, trazendo obj salvo
+	   	
+		andView.addObject("pessoas", pessoasIt); //passa pra lista o obj salvo
+		andView.addObject("pessoaObj", new Pessoa()); 
     	
     	 return andView;
      }
@@ -47,12 +51,10 @@ public class PessoaController {
      @RequestMapping(method = RequestMethod.GET, value="/listapessoas")
      public ModelAndView pessoas() { //metodo model e view especifica qual a view que será renderizada e quais dados vai usar    	 
      
-     
     	 ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
     	 Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
     	 andView.addObject("pessoas", pessoasIt);
     	 andView.addObject("pessoaObj", new Pessoa());
-
  
     	 return andView;
      
@@ -63,15 +65,56 @@ public class PessoaController {
     	 
     	 ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
     	 
-    	 Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
-    	 andView.addObject("pessoaObj", pessoa.get());
+    	 Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa); //carrega objeto pessoa do servidor buscando pelo id
+    	 andView.addObject("pessoaObj", pessoa.get()); // passa o objeto pessoa pelo parametro pessoaObj 
     	 
     	 return andView;
     	 
     	 
      }
      
+     @GetMapping("/excluirpessoa/{idpessoa}")
+     public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
+    	 
+    	 pessoaRepository.deleteById(idpessoa); // deleta o id passado por parametro	
+    	 
+    	 ModelAndView andView = new ModelAndView("cadastro/cadastropessoa"); // retorna pra essa view
+    	 andView.addObject("pessoas", pessoaRepository.findAll()); // retorna todos passando a lista atual (apos delete)
+    	 
+    	 andView.addObject("pessoaObj", new Pessoa()); //retorna obj vazio, limpo na tela(form)
+    	 return andView;
+    	 
+     
+     }
+     
+     @PostMapping("**/pesquisarpessoa") // intercepta
+     public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) { //recebe o param enviado pelo campo no front
+    	 
+    	 ModelAndView andView = new ModelAndView("cadastro/cadastropessoa"); // retorna pra essa view
+    	 
+    	 andView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa)); // adc o objeto pessoas o que foi buscado
+    	 andView.addObject("pessoaObj", new Pessoa());
+    	 
+    	 return andView;
+     }
+     
+     @GetMapping("/telefones/{idpessoa}")//intercepta dados enviados pela url metodo editar e id
+     public ModelAndView telefones(@PathVariable("idpessoa") Long idpessoa){ //tratar e retornar para a view
+    	 
+    	 Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa); //carrega objeto pessoa do servidor buscando pelo id
+    	 
+    	 ModelAndView andView = new ModelAndView("cadastro/telefones"); // retorna para tela de telefones
+    
+    	 andView.addObject("pessoaObj", pessoa.get()); // passa o objeto pessoa pelo parametro pessoaObj 
+    	 
+    	 return andView;
+    	 
+    	 
+     }
+     
+     
 }
+
 
 
 
--- src/main/java/com/springboot/repository/PessoaRepository.java
+++ src/main/java/com/springboot/repository/PessoaRepository.java
@@ -1,6 +1,10 @@
 package com.springboot.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +13,7 @@ import com.springboot.model.Pessoa;
 @Repository
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long>{ // pede uma classe e um valor de chave primaria
-
-	
+	@Transactional
+	@Query(value = "select p from Pessoa p where p.nome like %:nome%") //usando parametros nomeados
+	 List<Pessoa> findPessoaByName(@Param("nome")String nome);
 }
